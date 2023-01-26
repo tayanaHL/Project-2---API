e3c5f273d20b58895bab00c7c21c2da89be18921
Tayana <tayana1498@gmail.com>
2023-01-26 13:37:50 -0700

    user can delete comments

--- Real-Estate/controllers/commentControllers.js
+++ Real-Estate/controllers/commentControllers.js
@@ -10,10 +10,8 @@ router.post('/:commentId', (req, res) => {
     const commentId = req.params.propertyId
     if (req.session.loggenIn) {
         req.body.author = req.session.userId
-    } else {
-        res.sendStatus(401)
-    }
-    Comments.findById(propertyId)
+const Comments = req.body
+    Property.findById(propertyId)
         .then(comment => {
             //create comment with req body//
             property.comments.push(req.body)
@@ -26,6 +24,30 @@ router.post('/:commentId', (req, res) => {
             console.log(err)
             res.status(400).json(err)
         })
+    } else {
+    res.sendStatus(401)
+    }
 })
-
-module.export = router
+//Delete router//
+router.delete('/delete/:propertyID/:commentId', (req, res) => {
+    const { propertyId, commentId } = req.params
+    Property.findById(propertyId)
+    .then(property => {
+        const Comments = property.comments.id(commentId)
+        console.log('this is the comment to be deleted: \n', Comments )
+        if (req.session.loggenIn) {
+            if (Comments.author == req.session.userId) {
+                Comments.remove()
+                property.save()
+                    res.sendStatus(204)
+            } else {
+                res.sendStatus(401)
+            }
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    })
+})
+module.exports = router
